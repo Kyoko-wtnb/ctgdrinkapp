1fa413aa03cf9dec9e0c78d380dcd37824311ff6
Kyoko Watanabe <k.watanabe@vu.nl>
2018-06-29 15:10:25 +0200

    task schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,9 @@ namespace ctgdrinkapp\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use fuma\Http\Controllers\Controller;
+use DB;
+use Mail;
 
 class Kernel extends ConsoleKernel
 {
@@ -26,5 +29,19 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+		$schedule->call(function(){
+			$devemail = config('app.devemail');
+			$rows = DB::select('SELECT vunetid, name, email, total FROM members LEFT JOIN (SELECT id0, SUM(total) AS total FROM (SELECT vunetid AS id0, -1*SUM(cost) AS total FROM drinks GROUP BY vunetid UNION ALL SELECT vunetid AS id0, SUM(deposit) AS total FROM deposits WHERE status!="CANCELLED" GROUP BY vunetid) GROUP BY id0) ON vunetid=id0 WHERE total<0');
+			foreach($rows as $row){
+				$data = [
+	              'name'=>$row->name,
+	              'total'=>$row->total
+	            ];
+				Mail::send('emails.NegativeDeposit', $data, function($m) use($devemail, $row){
+	              $m->from($devemail, "CTG Drink Tracker");
+	              $m->to($row->email, $row->name)->subject("CTG Drink Tracker: Negative balance");
+	            });
+			}
+		});
     }
 }
---
+++ resources/views/emails/NegativeDeposit.blade.php
@@ -0,0 +1,7 @@
+<html>
+<head><h3>CTG Drink Deposit</h3></head>
+<body>
+	Hi {{ $name }},<br/>
+	Your balance is negative (total balance: {{ $total }}),
+	please transfer a deposite to Stephanie as soon as possible.<br/>
+</body>
