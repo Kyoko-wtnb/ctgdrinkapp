74727ffde75a3970820ba0207e57a2347cd63f2b
Kyoko Watanabe <k.watanabe@vu.nl>
2018-08-28 10:05:24 +0200

    email url update

--- resources/views/emails/deposit.blade.php
+++ resources/views/emails/deposit.blade.php
@@ -3,7 +3,7 @@
 <body>
 	Hi Stephanie,<br/>
 	{{ $name }} has deposited {{ $deposit }} euros.<br/>
-	Please check your account and confirm by clicking <a href="http://kyoko.ctglab.nl/ctgdrinkapp/deposit/confirm/{{ $id }}">CONFIRM</a>.<br/>
-	If you don't find the corresponding transaction, please decline by clicking <a href="http://kyoko.ctglab.nl/ctgdrinkapp/deposit/cencel/{{ $id }}">DECLINE</a>.<br/>
+	Please check your account and confirm by clicking <a href="http://drinks.ctglab.nl/deposit/confirm/{{ $id }}">CONFIRM</a>.<br/>
+	If you don't find the corresponding transaction, please decline by clicking <a href="http://drinks.ctglab.nl/deposit/cencel/{{ $id }}">DECLINE</a>.<br/>
 	Thank you!!
 </body>
